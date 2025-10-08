5f09feabe9af2b3965f0b8582288add95310f666
leonel <engr.leonel.calderon.pece.mse.ect.ae@gmail.com>
2025-10-09 00:15:10 +0800

    Add week 4 files

---
+++ Week 4 Familiarization with PyCharm IDE/myFirstPythonFile.py
@@ -0,0 +1,12 @@
+
+#convert to int -> int(variable)
+#convert to string -> str(variable)
+#fstring or formatted string
+
+myAge = 45
+myName = "Leonel Calderon"
+
+#print("I am : " + myName + " and my age is : " + myAge)
+print("I am : " + myName + " and my age is : " + str(myAge))
+print(f"I am : {myName} and my age is : {myAge}")
+
